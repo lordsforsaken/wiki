bc66ed8f0ca04388ff5754c5e4632d383dee4ef2
AusPrinzip <cdmdb@hotmail.com>
2024-07-27 21:41:06 -0600

    added twitter metatags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,25 @@
 import 'nextra-theme-docs/style.css';
 
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+// pages/_app.js
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* General meta tags */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* Twitter Card meta tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@lordsforsak3n" />
+        <meta name="twitter:title" content="Lords Forsaken Wiki" />
+        <meta name="twitter:description" content="Explore the comprehensive wiki for the game Lords Forsaken." />
+        <meta name="twitter:image" content="https://dvaiulh40vjp3.cloudfront.net/logos-icons/black-logo.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
